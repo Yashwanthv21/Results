04d8990cbea80bba2d5f98cd0d556cb2dae9235d
Yashwanth Reddy <you@example.com>
2016-08-20 17:34:55 +0530

    Push final

--- Results/settings.py
+++ Results/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#69+006yy2+q#synno38@-q*aezl^a6r@@ulc9k@&anp1-b2@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
